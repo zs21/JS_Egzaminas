ec69450a7148368c58099b8a9fd5f1cb60dc4259
Žygimantas <zygimantas.stonkus@gmail.com>
2022-05-17 10:19:47 +0300

    finished task 3

--- Task 3/script.js
+++ Task 3/script.js
@@ -14,38 +14,34 @@ turėti bent minimalų stilių ir būti responsive;
 const ENDPOINT = "https://api.github.com/users";
 
 let outputEl = document.getElementById("output");
-
+let messegeEl = document.getElementById("message");
 let buttonEl = document.getElementById("btn");
 buttonEl.addEventListener("click", handleSubmit);
 
 async function handleSubmit(e) {
   e.preventDefault();
+  messegeEl.style.display = "none";
+  const usersData = await getUsersData();
+  //   console.log(usersData);
 
-  const usersData = getUsersData();
-  console.log(usersData);
-
-  //   updateDOM(usersData);
-  console.log(getUsersData);
+  updateDOM(usersData);
 }
 
 async function getUsersData() {
   const response = await fetch(ENDPOINT);
   const data = await response.json();
-  console.log(data.forEach((element) => console.log(element)));
   return data;
 }
 
-// async function updateDOM(data) {
-//   await data.forEach(element);
-//   {
-//     createEl(element);
-//   }
-// }
-
-// function createEl(obj) {
-//   const newP = document.createElement("p");
-//   newP.classList.add("newp");
-//   newP.textContent = `${obj.login}, ${obj.avatar_url}`;
-// }
-
-console.log(getUsersData[5]);
+async function updateDOM(data) {
+  await data.forEach((user) => {
+    let userCard = document.createElement("div");
+    userCard.textContent = user.login;
+    let avatar = document.createElement("img");
+    avatar.src = user.avatar_url;
+    userCard.classList.add("userCard");
+    avatar.classList.add("avatar");
+    outputEl.appendChild(userCard);
+    userCard.appendChild(avatar);
+  });
+}
