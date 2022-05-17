167cdd43dec09c623e25637721bc76f204149333
Žygimantas <zygimantas.stonkus@gmail.com>
2022-05-17 09:34:15 +0300

    Finished task 4

--- Task 4/script.js
+++ Task 4/script.js
@@ -15,19 +15,23 @@ let outputEl = document.getElementById("output");
 
 const carsData = getCarsData();
 
-updateDOM(carsData);
-
 async function getCarsData() {
   const response = await fetch(ENDPOINT);
   const data = await response.json();
+  updateDOM(data);
   return data;
 }
 
-console.log(getCarsData());
+// console.log(carsData);
 
 async function updateDOM(data) {
-  await console.log(data[0]);
-  const brandCard = document.createElement("div");
-  brandCard.textContent = data[0];
-  outputEl.appendChild(brandCard);
+  await data.forEach((brand) => {
+    let brandCard = document.createElement("div");
+    brandCard.textContent = brand.brand;
+    brandCard.classList.add("brandCard");
+    let brandCardModels = document.createElement("p");
+    brandCardModels.textContent = brand.models;
+    outputEl.appendChild(brandCard);
+    brandCard.appendChild(brandCardModels);
+  });
 }
